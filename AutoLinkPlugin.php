0ddfe4114e9e7cbb393878b8f9b4786a064c79b8
tijsvanerp <tijsvanerp@gmail.com>
2017-03-21 14:17:20 +0100

    Checking in changes prior to tagging of version 1.1.7.

--- AutoLinkPlugin.php
+++ AutoLinkPlugin.php
@@ -12,7 +12,7 @@ use ReflectionClass;
  * @link      http://theconceptstore.nl
  * @package   AutoLink
  * @since     1.0.0
- * @VERSION 1.1.6
+ * @VERSION 1.1.7
  */
 class AutoLinkPlugin extends BasePlugin
 {
